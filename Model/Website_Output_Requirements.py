890ecce7cbfc86283e13218532634d4f0eb445f3
Akurati Hemanth <129819031+AkuratiHemanth@users.noreply.github.com>
2023-08-16 19:28:30 +0530

    Update Website_Output_Requirements.py

--- Model/Website_Output_Requirements.py
+++ Model/Website_Output_Requirements.py
@@ -15,12 +15,13 @@ def label_sentiment(sentiment_score):
     return "neutral"
 
 def generate_wordcloud(text):
-    wc = WordCloud(max_words=1000, width=800, height=400, collocations=False).generate(text)
+    wc = WordCloud(max_words=1000, width=800, height=400, collocations=False, background_color=None).generate(text)
     plt.figure(figsize=(8, 4))
     plt.imshow(wc, interpolation="bilinear")
     plt.axis("off")
     image_path = "static/wordcloud.png"
-    plt.savefig(image_path)
+    plt.savefig(image_path, transparent=True)  
     plt.close()
     return image_path
+
 
