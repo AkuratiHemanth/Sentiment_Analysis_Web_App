5a1cee0d4f7d9129411a4e700fbcb07e3fdf1fbf
Akurati Hemanth <129819031+AkuratiHemanth@users.noreply.github.com>
2023-08-16 08:22:15 +0530

    Update Website_Output_Requirements.py

--- Model/Website_Output_Requirements.py
+++ Model/Website_Output_Requirements.py
@@ -1,1 +1,26 @@
+from textblob import TextBlob
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+def analyze_sentiment(tweet):
+    analysis = TextBlob(tweet)
+    sentiment_score = analysis.sentiment.polarity
+    return sentiment_score
+
+def label_sentiment(sentiment_score):
+    if sentiment_score < -0.05:
+        return "negative"
+    if sentiment_score > 0.35:
+        return "positive"
+    return "neutral"
+
+def generate_wordcloud(text):
+    wc = WordCloud(max_words=1000, width=800, height=400, collocations=False).generate(text)
+    plt.figure(figsize=(8, 4))
+    plt.imshow(wc, interpolation="bilinear")
+    plt.axis("off")
+    image_path = "static/wordcloud.png"
+    plt.savefig(image_path)
+    plt.close()
+    return image_path
 
