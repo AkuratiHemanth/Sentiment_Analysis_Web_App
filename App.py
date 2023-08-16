72d753958f90271f4ae252aa448cf9316555b2f2
Akurati Hemanth <129819031+AkuratiHemanth@users.noreply.github.com>
2023-08-16 08:12:10 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,25 @@
+from flask import Flask, render_template, request
+from sentiment_analysis import label_sentiment, generate_wordcloud
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    sentiment_result = None
+    wordcloud_img = None
+
+    if request.method == "POST":
+        user_tweet = request.form["user_tweet"]
+        
+        sentiment_score = analyze_sentiment(user_tweet)
+        sentiment_result = label_sentiment(sentiment_score)
+
+        wordcloud_img = generate_wordcloud(user_tweet)
+
+    return render_template("index.html",
+                           sentiment_result=sentiment_result,
+                           wordcloud_img=wordcloud_img)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
